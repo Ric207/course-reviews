237c7e0e18bca87331f2d099fb8065782396ee83
Ric207 <ericngumi01@gmail.com>
2026-01-21 00:49:39 -0800

    Fix dashboard URL routing

--- courses/urls.py
+++ courses/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     # Homepage
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-    path("contact/", views.contact_us, name="contact_us"),    
+    path("contact/", views.contact_us, name="contact_us"), 
+     path("owner/dashboard/", views.owner_dashboard, name="owner_dashboard"),
 ]
 
 
